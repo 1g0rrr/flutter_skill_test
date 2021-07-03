fad00a2e1455ad622e88047eadcd8efb9178e0bc
Igor <1g0rrr.my@gmail.com>
2021-07-03 14:47:51 +0300

    add loader to images

--- lib/image_part.dart
+++ lib/image_part.dart
@@ -13,6 +13,16 @@ class ImagePart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Image.network(
       recipe.pictureUrl,
+      loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+        if (loadingProgress == null) {
+          return child;
+        }
+        return Center(
+          child: CircularProgressIndicator(
+            value: loadingProgress.expectedTotalBytes != null ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes! : null,
+          ),
+        );
+      },
     );
   }
 }
